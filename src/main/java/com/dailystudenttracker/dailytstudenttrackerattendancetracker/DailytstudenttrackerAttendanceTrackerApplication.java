81ebd86a158899ad2d32f1ee14f67785f71c2525
laura <laura@codeup.com>
2023-02-08 11:51:52 -0600

    initial commit

---
+++ src/main/java/com/dailystudenttracker/dailytstudenttrackerattendancetracker/DailytstudenttrackerAttendanceTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dailystudenttracker.dailytstudenttrackerattendancetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailytstudenttrackerAttendanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailytstudenttrackerAttendanceTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dailystudenttracker/dailytstudenttrackerattendancetracker/DailytstudenttrackerAttendanceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailystudenttracker.dailytstudenttrackerattendancetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailytstudenttrackerAttendanceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
